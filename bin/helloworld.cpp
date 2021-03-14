b935048704ae078e391678d0866a4daef69be87f
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2021-03-13 20:24:30 -0500

    Music plays immediately when you start the game

--- bin/helloworld.cpp
+++ bin/helloworld.cpp
@@ -1,27 +1,36 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 int main(int argc, char** argv)
 {
-  // create main window
-    sf::RenderWindow App(sf::VideoMode(800,600,32), "Quick Quisine", sf::Style::Titlebar | sf::Style::Close);
+  // Create main window
+  sf::RenderWindow app(sf::VideoMode(800,600,32), "Quick Quisine", sf::Style::Titlebar | sf::Style::Close);
+  sf::Music music;
 
-  // start main loop
-  while(App.isOpen())
+  // Open music file and play music 
+  // We open instead of loading to use less memory
+  if (!music.openFromFile("../data/ChillingMusic.wav")) 
+      std::cout << "Could not load music." << std::endl;
+  music.play();
+
+  // Start main loop
+  while(app.isOpen())
   {
-    // process events
+    // Process events
     sf::Event Event;
-    while(App.pollEvent(Event))
+    while(app.pollEvent(Event))
     {
       // Exit
       if(Event.type == sf::Event::Closed)
-        App.close();
+        app.close();
     }
 
-    // clear screen and fill with blue
-    App.clear(sf::Color::Blue);
+    // Clear screen and fill with blue
+    app.clear(sf::Color::Blue);
 
-    // display
-    App.display();
+    // Display
+    app.display();
   }
 
   // Done.
