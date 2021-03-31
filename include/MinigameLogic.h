946fa2724ae979fba34aa413a7bfef0d043dfc40
David S <dmsolinsky@email.wm.edu>
2021-03-31 01:56:58 -0400

    update

--- include/MinigameLogic.h
+++ include/MinigameLogic.h
@@ -5,9 +5,16 @@
 class MinigameLogic : std::enable_shared_from_this<MinigameLogic>
 {
 private:
-    
+    int currentGame;
+    std::shared_ptr<Song> song;
+    std::vector<float> userTimings;
+    int score;
+    int correctTimingPos;
+    int difficult;
 public:
     MinigameLogic(/* args */);
+    int determineHitAccuracy();
+    std::vector<float> computerNextInt(float& time);
     virtual void move(const float& dt) = 0;
 };
 #endif
